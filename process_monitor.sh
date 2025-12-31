7ede0ee0bb9770c49b97140eb165b9c2a02d2ad1
Brian Gelhorn <briangelhorn@gmail.com>
2025-12-31 10:00:16 -0300

    Fixed minor bugs and changed the "Process not found" algorythm detection

--- process_monitor.sh
+++ process_monitor.sh
@@ -50,15 +50,13 @@ if [[ -z "$PROC" ]]; then
 fi
 #				 1   2    3    4    5    6 
 PROCESSLIST=$(ps --no-headers -o pid,comm,%cpu,%mem,user,stat -C "$PROC")
-STATUS=$?
 HEADER="PID NAME"
 FIELDS="1 2"
-if [[ "$STATUS" -ne 0 ]]; then
+if [[ -z "$PROCESSLIST" ]]; then
 	echo No process was found
 	exit 1
 fi 
 echo List of processes labeled "$PROC"\:
-
 if [[ "$METRICS" == true ]]; then
 	HEADER+=" CPU RAM"	
 	FIELDS+=" 3 4"
@@ -73,9 +71,8 @@ if [[ "$PROCESSTATUS" == true ]]; then
 	HEADER+=" STATUS"
 	FIELDS+=" 6"
 fi
-echo $HEADER
-echo "$PROCESSLIST" | 
-	awk -v fields="$FIELDS" '{
+echo "$HEADER"
+echo "$PROCESSLIST" | awk -v fields="$FIELDS" '{
 	n = split(fields, field, " ")
 	for(i=1; i<=n; i++)
 	{
